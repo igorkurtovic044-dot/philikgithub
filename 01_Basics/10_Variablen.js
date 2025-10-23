5a565ae48937cfc95498306f3c253a9feeefc1d1
philthymf <igorkurtovic044@gmail.com>
2025-10-23 13:03:57 +0200

    init

---
+++ 01_Basics/10_Variablen.js
@@ -0,0 +1,13 @@
+console.log("hallo")
+console.log("Tolle Sache")
+let a = 7
+let b = 8
+let c = 10
+console.log(a + b + c)
+console.log((a + b) / c)
+console.log(c - a + b)
+console.log(a + b + c * 3)
+console.log((a + b + c) / 4)
+let a = 3
+let b = a + 3
+let c = c - 3
---
+++ 01_Basics/20_Strings.js
@@ -0,0 +1,3 @@
+let a = 17.4
+let b = 4.4
+let result =
