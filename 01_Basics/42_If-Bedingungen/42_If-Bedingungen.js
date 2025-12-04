d62d93de05068765c7c4c3e352ce7f681a11002f
philthymf <igorkurtovic044@gmail.com>
2025-12-04 11:58:41 +0100

    move

---
+++ 01_Basics/42_If-Bedingungen/42_If-Bedingungen.js
@@ -0,0 +1,8 @@
+let randomNumber = Math.floor(Math.random() * 10)
+if (randomNumber < 5) {
+    console.log("The Number is less than 5")
+} else if (randomNumber > 5) {
+    console.log("The number is bigger than 5")
+} else if (randomNumber == 5) {
+    console.log("The Number is 5")
+}
