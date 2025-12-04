981028cd1109772cdaa490033dc84cf583dd8da9
philthymf <igorkurtovic044@gmail.com>
2025-12-04 11:59:06 +0100

    move

---
+++ 01_Basics/10_Variablen_Strings/10_Variablen.js
@@ -0,0 +1,13 @@
+console.log("hallo")
+console.log("Tolle Sache")
+let a = 7
+let b = 8
+let c = 10
+console.log(a + b + c)
+console.log((a + b) / c)
+console.log(c - a + b)
+console.log(a + b + c * 3)
+console.log((a + b + c) / 4)
+a = 3
+b = a + 3
+c = c - 3
