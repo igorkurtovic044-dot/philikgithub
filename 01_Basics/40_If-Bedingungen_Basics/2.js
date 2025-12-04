d955e67fa9f69b8a0c0a307c3a644a0c4f322b77
philthymf <igorkurtovic044@gmail.com>
2025-12-04 11:52:11 +0100

    40 Aufgabe 2

---
+++ 01_Basics/40_If-Bedingungen_Basics/2.js
@@ -0,0 +1,13 @@
+trafficlight = "yellow"
+if (trafficlight == "green") {
+    console.log("You may go")
+
+}
+
+if (trafficlight == "yellow") {
+    console.log("Caution")
+}
+
+if (trafficlight == "red") {
+    console.log("Stop")
+}
