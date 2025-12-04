dd9a2d116016e23789bbd360119147b346bea12f
philthymf <igorkurtovic044@gmail.com>
2025-12-04 11:51:43 +0100

    41 Aufgabe 1

---
+++ 01_Basics/41_If-Aufgaben.js
@@ -0,0 +1,20 @@
+a = 2
+if (a > 0) {
+    console.log("The Number is positive")
+} else {
+    console.log("The number is not positive")
+}
+
+
+if (a < 0) {
+    console.log("The number is negativ")
+} else {
+    console.log("The number is positiv or 0")
+}
+
+
+if (a = 0) {
+    console.log("The nummber is zero")
+} else {
+    console.log("The Number is not zero")
+}
