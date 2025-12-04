4a14884ea4638b9587efdf9ade087a7981206b96
philthymf <igorkurtovic044@gmail.com>
2025-12-04 11:51:32 +0100

    40 Aufgabe 1

--- 01_Basics/40_If-Bedingungen_Basics/1.js
+++ 01_Basics/40_If-Bedingungen_Basics/1.js
@@ -1,1 +1,15 @@
-a = 888
+a = 1
+if (a > 0) {
+
+    console.log("The Number is positive")
+}
+
+if (a < 0) {
+
+    console.log("The Number is negative")
+}
+
+if (a == 0) {
+
+    console.log("The number is zero")
+}
