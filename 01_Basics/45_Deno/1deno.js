84cc77008f1e2bc4acf9cda97003c27967f55c22
philthymf <igorkurtovic044@gmail.com>
2025-12-04 12:47:30 +0100

    Deno erste aufgabe abu hamza

---
+++ 01_Basics/45_Deno/1deno.js
@@ -0,0 +1,9 @@
+let input = prompt("Gib dein PASSWORT ein");
+let password = ("password");
+console.log(input);
+
+if (input == password) {
+    console.log("Acces granted");
+} else if (password !== input) {
+    console.log("Acces not granted");
+}
