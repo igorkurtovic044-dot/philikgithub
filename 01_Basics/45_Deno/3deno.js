3b3a7900a4e2baefbc8e447f02ede259a52c4762
philthymf <igorkurtovic044@gmail.com>
2025-12-11 12:31:04 +0100

    Dritte Deno

---
+++ 01_Basics/45_Deno/3deno.js
@@ -0,0 +1,20 @@
+prompt("Say two Numbers and i will calculate Them for you");
+let num1 = Number(prompt("First Number:"));
+let num2 = Number(prompt("Second Number:"));
+let operation = prompt("Choose an operation: +, -, *, /");
+
+if (operation == "+") {
+    console.log("The result is: " + (num1 + num2));
+}
+else if (operation == "-") {
+    console.log("The result is: " + (num1 - num2));
+}
+else if (operation == "*") {
+    console.log("The result is: " + (num1 * num2));
+}
+else if (operation == "/") {
+    console.log("The result is: " + (num1 / num2));
+}
+else {
+    console.log("Invalid operation");
+}
