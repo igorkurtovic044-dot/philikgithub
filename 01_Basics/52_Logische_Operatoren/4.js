2367b60a25d8b2cc90235e0093c0b83d6fca1fcb
philthymf <igorkurtovic044@gmail.com>
2025-12-11 16:04:16 +0100

    4 52_Logische_Operatoren

---
+++ 01_Basics/52_Logische_Operatoren/4.js
@@ -0,0 +1,11 @@
+let age = Number(prompt("Enter your age:"));
+let money = Number(prompt("Enter the amount of money you have:"));
+prompt = (age)
+prompt = (money);
+if (age >= 18 && money >= 12) {
+    console.log("Tikcet purchased");
+} else if (age >= 18 && money < 12) {
+    console.log("Not enough money ");
+} else {
+    console.log("You are too young to watch this movie");
+}
