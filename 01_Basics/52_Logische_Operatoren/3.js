303c41b5eed674b39cb1f610f6e4edd4bc0c4900
philthymf <igorkurtovic044@gmail.com>
2025-12-11 15:44:42 +0100

    3 52_Logische_Operatoren repaired

--- 01_Basics/52_Logische_Operatoren/3.js
+++ 01_Basics/52_Logische_Operatoren/3.js
@@ -1,6 +1,9 @@
 let grade = Number(prompt("Enter your grade 0-100:"));
-let attendance = Number(prompt("Enter your attendance  0-100):"));
-let extraCredit = prompt("Did you complete extra credit? 0-10:");
+let attendance = Number(prompt("Enter your attendance  0-100:"));
+let extraCredit = prompt("Did you complete extra credit? yes/no:");
+prompt = (grade)
+prompt = (attendance)
+prompt = (extraCredit);
 if (grade > 90 && attendance > 80 || extraCredit === "yes") {
     console.log("Eligible for advanced exam");
 } else {
