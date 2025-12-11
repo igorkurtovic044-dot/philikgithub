4d9d551b85925e381c3fe71993647e595df75dcf
philthymf <igorkurtovic044@gmail.com>
2025-12-11 13:04:08 +0100

    6deno

---
+++ 01_Basics/45_Deno/6deno.js
@@ -0,0 +1,14 @@
+prompt("What drink would you like");
+let drink = prompt("Enter your drink choice (cola/water/tea/beer):");
+if (drink === "cola") {
+    console.log("Thanks for choosing cola!");
+}
+else if (drink === "water") {
+    console.log("Thanks for choosing water!");
+}
+else if (drink === "tea") {
+    console.log("Thanks for choosing tea!");
+}
+else if (drink === "beer") {
+    console.log("Thanks for choosing beer!");
+}
