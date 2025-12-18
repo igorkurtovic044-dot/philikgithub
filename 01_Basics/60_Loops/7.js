60ba61c8a0140c40145fcadd43d456c597761dfd
philthymf <igorkurtovic044@gmail.com>
2025-12-18 12:54:11 +0100

    60_Loops 7

---
+++ 01_Basics/60_Loops/7.js
@@ -0,0 +1,7 @@
+console.log("im going to find all the numbers from 1-100 that are dividible by 5");
+for (let i = 1; i <= 100; i++) {
+    if (i % 5 === 0) {
+        console.log(i);
+    }
+}
+console.log("These are all the numbers from 1-100 that are dividible by 5");
